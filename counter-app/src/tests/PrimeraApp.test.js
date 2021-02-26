cf6b6ebeeacfccb19eddf8405c0e780bdba2990c
Amaury Soto <amaurysotog@gmail.com>
2021-02-25 19:27:29 -0500

    I still have one error with adapter from enzyme

--- counter-app/src/tests/PrimeraApp.test.js
+++ counter-app/src/tests/PrimeraApp.test.js
@@ -1,20 +1,41 @@
 import React from 'react'
 import { render } from "@testing-library/react"
 import PrimeraApp from "../PrimeraApp"
+import Enzyme from 'enzyme';
+import { shallow } from 'enzyme'
 
 describe('Pruebas Comp <PrimeraApp/>', () => {
   
-  test('Debe mostrar msj Hola Amaury ', () => {
-    
+/*   test('Debe mostrar msj Hola Amaury ', () => {
+
+      //We need render the component 
+      //This message to evalued
       const saludo = 'Hola Amaury'
-      const { getByText } = render(<PrimeraApp saludo={saludo} />)
+
+      //This we can use method render from  testing-library //
+      const {getByText}= render(<PrimeraApp saludo={saludo} />)
+    
+
+
+     
+     
 
       
-
+      //Method toBeInTheDocument from import setupTests.js
+      //Here only test about if exist saludo in this component
       expect( getByText(saludo) ).toBeInTheDocument()
 
 
+  }) */
+
+
+  test('PrimeraApp correctamente <PrimeraApp/> ', () => {
+
+    const saludo = 'Hola Amaury'
+    const wrapper = shallow(<PrimeraApp saludo={saludo} />)
+    
   })
+  
   
 
 })
