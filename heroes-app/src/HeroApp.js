374518f8d0d45e7ed2860a38e5e0ea37417a8d6b
Amaury Soto <amaurysotog@gmail.com>
2021-03-26 17:57:49 -0500

    Initial Hero-App

---
+++ heroes-app/src/HeroApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeroApp = () => {
+  return (
+    <div>
+      <h1>Hero-App</h1>
+      <hr />
+    </div>
+  );
+};
+
+export default HeroApp;
---
+++ heroes-app/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import HeroApp from "./HeroApp";
+
+ReactDOM.render(<HeroApp />, document.getElementById("root"));
