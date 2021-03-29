dafbf40f1b0cda886a2001394668e1c7e92debac
Amaury Soto <amaurysotog@gmail.com>
2021-03-29 18:57:37 -0500

    List Hero render and new Desinf

--- heroes-app/src/components/heroes/HeroCard.js
+++ heroes-app/src/components/heroes/HeroCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./HeroCard.css";
 
 const HeroCard = ({
   id,
@@ -10,30 +11,29 @@ const HeroCard = ({
   characters,
 }) => {
   return (
-    <div className="card ms-3" style={{ maxWidth: "540px" }}>
-      <div className="row no-gutters">
-        <div className="col-md-4">
-          <img
-            src={`./assets/heroes/${id}.jpg`}
-            className="card-img"
-            alt={superhero}
-          />
-        </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <h5 className="card-title">{superhero}</h5>
-            <p className="card-text">{alter_ego}</p>
-            {alter_ego !== characters && (
-              <p className="card-text">{characters}</p>
-            )}
-            <p className="card-text">
-              <small className="text-muted">{first_appearance}</small>
-            </p>
-            <Link to={`./hero/${id}`}>More...</Link>
+    <Link to={`./hero/${id}`} className="my-card">
+      <img
+        src={`./assets/heroes/${id}.jpg`}
+        className="img img-responsive"
+        alt={superhero}
+      />
+      <div className="profile-name">{superhero}</div>
+      <div className="profile-position">{alter_ego}</div>
+      <div className="profile-overview">
+        <div className="profile-overview">
+          <div className="row">
+            <div className="col-ms-4">
+              <h3>{publisher}</h3>
+              <p>
+                Primera aparición: <br />
+                {first_appearance}
+              </p>
+              {alter_ego !== characters && <p>{characters}</p>}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
