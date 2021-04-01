973ff224e8fbb80e29e882af23626cb4bc8bb952
Amaury Soto <amaurysotog@gmail.com>
2021-04-01 14:15:08 -0500

    Test in PrivatRoute ADD IN LOCALSTORAGE class 200

--- heroes-app/src/tests/routes/PrivateRoute.test.js
+++ heroes-app/src/tests/routes/PrivateRoute.test.js
@@ -4,20 +4,24 @@ import PrivateRoute from "../../routers/PrivateRoute"
 
 describe('Test in PrivateRoute', () => {
 
+  //create props with values required
   const props = {
     location: {
       pathname: '/marvel'
     }
   }
 
+  Storage.prototype.setItem = jest.fn()
+
   test('Debe mostrar componente si está autenticado y guardar localStorage', () => {
-    //usamos mount en lugar de shallow, debido a que mount es mas profundo
+    //usamos mount en lugar de shallow, debido a que mount es mas profundo y pueda renderizar el PrivateRoute
     const wrapper = mount(
       // para hacer pruebas de routers used MemoryRouter from react-router
       <MemoryRouter>
 
         <PrivateRoute
           isAuthenticated={true}
+          //De esta manera simulamos un component.. es una arrow function que retorna un tag html. en este caso un jsx span
           component={() => <span></span>}
           {...props}
 
@@ -26,6 +30,7 @@ describe('Test in PrivateRoute', () => {
     )
 
     expect(wrapper.find('span').exists()).toBe(true)
+    expect(localStorage.setItem).toHaveBeenCalledWith('lastPathname', '/marvel')
 
   })
 
