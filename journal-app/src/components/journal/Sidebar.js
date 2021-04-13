2ba111b0a3e190d35ecec270c2ea6c88bbfaf342
Amaury Soto <amaurysotog@gmail.com>
2021-04-13 17:04:15 -0500

    Add name in sidebar 258

--- journal-app/src/components/journal/Sidebar.js
+++ journal-app/src/components/journal/Sidebar.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { startLogout } from '../../actions/auth'
 import JournalEntries from './JournalEntries'
 
 const Sidebar = () => {
 
   const dispatch = useDispatch()
+  const { name } = useSelector(state => state.auth)
 
   const handleLogout = (e) => {
     e.preventDefault()
@@ -20,7 +21,7 @@ const Sidebar = () => {
       <div className="journal__sidebar-navbar">
         <h3 className="mt-5">
           <i className="far fa-moon"></i>
-          <span> Amaury</span>
+          <span> {name}</span>
         </h3>
         <button className="btn" onClick={handleLogout}>Logout</button>
       </div>
