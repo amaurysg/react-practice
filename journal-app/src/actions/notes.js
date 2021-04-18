53cf6e91af6d6993b61575c4e3c6912c617106b2
Amaury Soto <amaurysotog@gmail.com>
2021-04-18 12:32:01 -0500

    Importante, repasar Activar note class 261

--- journal-app/src/actions/notes.js
+++ journal-app/src/actions/notes.js
@@ -1,6 +1,7 @@
 import { db } from "../firebase/config"
+import types from "../types/types"
 
-
+//ACTIONS NOTES !!! 
 export const startNewNote = () => {
   //getState es como el useSelector, nos da acceso al state
   //getState es una funcion y un  segundo parametro en una funcion asincrona 
@@ -8,20 +9,29 @@ export const startNewNote = () => {
   return async (dispatch, getState) => {
     //tomamos el uid de firebase gracias getState
     const uid = getState().auth.uid
-    console.log(uid)
+    // console.log(uid)
 
     //creamos objeto 
-
     const newNote = {
-      title: '',
+      title: 'Hola nena',
       body: '',
       date: new Date().getTime()
     }
     //usabos db {firebase} .collection y asignamos ruta y colecciones 
     //que usaremos, notese que la primera es el id del user
     const doc = await db.collection(`${uid}/journal/notes`).add(newNote)
-    console.log(doc)
+    console.log(doc.id)
 
-
+    dispatch(activeNote(doc.id, newNote))
   }
 }
+
+export const activeNote = (id, note) => ({
+  type: types.notesActive,
+  payload: {
+    id,
+    ...note,
+  }
+
+
+})
